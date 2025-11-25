0e9c41f68f471199f23b7fb706e30d810ddc6969
Croudxd <benjamincroud@gmail.com>
2025-11-25 01:32:12 +0000

    Added function to calculate SMA from a vector full of DailyQuote objects.

---
+++ src/engine/indicators/sma.rs
@@ -0,0 +1,11 @@
+use crate::engine::model::daily_quote::DailyQuote;
+
+pub fn calc_sma(window: Vec<DailyQuote>) -> f64
+    {
+        //iterate over current window, calculate average
+        let mut sum: f64 = 0.0;
+        for x in &window{
+            sum += x.close;
+        }
+        return sum / window.len() as f64;
+    }
